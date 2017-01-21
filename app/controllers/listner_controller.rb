95f768b99e4980ab3e86e397adc0027077165d35
dushyanthi pieris <dushi27@gmail.com>
2017-01-21 17:25:28 -0500

    add token

--- app/controllers/listner_controller.rb
+++ app/controllers/listner_controller.rb
@@ -1,15 +1,15 @@
 require 'json'
 class ListnerController < ApplicationController
 	def event_triggered	
-		response.headers['X-OUTBOUND-TOKEN'] = 'HMAC-SHA256'
-		# @secret = ENV.fetch('WHS', 'test secret')
+		@secret = ENV.fetch('WHS', 'test secret')
+		response.headers['X-OUTBOUND-TOKEN'] = @secret
+		
 		# render :json =>  {status: 200, type: 'verification', @secret}
 		# {"type":"verification","challenge":"xxx"}
 		# information = request.raw_post
 		# data_parsed = JSON.parse(information)
 		# raise response.body.inspect
-		raise params['challenge'].inspect
-		render :json =>  params['challenge'].gsub("(").gsub(")")
+		render :json =>  params['challenge']
 	end
 
 end
