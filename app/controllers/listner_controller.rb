83dde5626054a25c6629e5f3363d6ae712258ae4
dushyanthi pieris <dushi27@gmail.com>
2017-01-21 17:12:25 -0500

    watson verification

--- app/controllers/listner_controller.rb
+++ app/controllers/listner_controller.rb
@@ -1,10 +1,16 @@
+require 'json'
 class ListnerController < ApplicationController
 	def event_triggered	
 		response.headers['X-OUTBOUND-TOKEN'] = 'HMAC-SHA256'
 		# @secret = ENV.fetch('WHS', 'test secret')
 		# render :json =>  {status: 200, type: 'verification', @secret}
 		# {"type":"verification","challenge":"xxx"}
+		# information = request.raw_post
+		# data_parsed = JSON.parse(information)
+		# raise response.body.inspect
 		render :json =>  params['challenge']
 	end
 
 end
+
+# 
