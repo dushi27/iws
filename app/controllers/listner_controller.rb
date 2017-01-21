a5acee41c4fc340321da2a786fcd36bbede891b2
dushyanthi pieris <dushi27@gmail.com>
2017-01-21 18:45:47 -0500

    reretry

--- app/controllers/listner_controller.rb
+++ app/controllers/listner_controller.rb
@@ -2,17 +2,16 @@ require 'json'
 class ListnerController < ApplicationController
 	def event_triggered	
 		@secret = ENV.fetch('WHS', 'test secret')
-		message = '{"response" : #{param[:challenge]}}'
+		message = {response: params['challenge']}
 
-		got_challenge = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), @secret, message)
-	
+		got_challenge = OpenSSL::HMAC.hexdigest("SHA256", @secret, message.to_json) 
 		puts got_challenge
 		puts "******************"
 		response.headers['X-OUTBOUND-TOKEN'] = got_challenge
 
 
 
-		render :json =>  {response: params['challenge']}
+		render :json =>  message
 	end
 
 end
